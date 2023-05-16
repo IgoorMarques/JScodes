c3585ffe5b07fe0f65511da6a38eaca46dbbfd5a
igor <igo12345marques@gmail.com>
2023-05-16 20:36:51 -0300

    salvando alterações

---
+++ filter.js
@@ -0,0 +1,36 @@
+const lista = [
+    {
+        nome: 'José',
+        idade: 20,
+    },
+    {
+        nome: 'Amanda',
+        idade: 23,
+    },
+    {
+        nome: 'Igor',
+        idade: 24,
+    },
+    {
+        nome: 'Iago',
+        idade: 20,
+    },
+    {
+        nome: 'Juan',
+        idade: 21
+    },
+    {
+        nome: 'Ilames',
+        idade: 26
+    },
+    {
+        nome: 'Lucas',
+        idade: 23
+    }
+]
+
+const listaFiltrada = lista.filter(
+    (pessoa) => pessoa.idade <= 20
+)
+
+console.log(listaFiltrada)
---
+++ forEach.js
@@ -0,0 +1,43 @@
+const lista = [
+    {
+        nome: 'José',
+        idade: 20,
+    },
+    {
+        nome: 'Amanda',
+        idade: 23,
+    },
+    {
+        nome: 'Igor',
+        idade: 24,
+    },
+    {
+        nome: 'Iago',
+        idade: 20,
+    },
+    {
+        nome: 'Juan',
+        idade: 21
+    },
+    {
+        nome: 'Ilames',
+        idade: 26
+    },
+    {
+        nome: 'Lucas',
+        idade: 23
+    }
+]
+
+
+//for(let i = 0; i <= lista.length; i++){
+//    console.log(lista[i])
+//}
+
+
+//Uso do forEach, veja a vantagem de usá-lo!
+
+lista.forEach((objeto)=>{
+    console.log(objeto)
+})
+
